1e59f81b097694f1b96e9f0cd17049c529413de7
Fofo <127132293+fk-crafter@users.noreply.github.com>
2024-02-14 17:16:39 +0100

    Update main.js

--- 13 -/main.js
+++ 13 -/main.js
@@ -1,11 +1,11 @@
 function toggleMenu() {
-    var menu = document.getElementById("hamburger-contents");
+    const menu = document.getElementById("hamburger-contents");
     menu.classList.toggle("show");
 }
 
 window.onclick = function(event) {
     if (!event.target.matches('#hamburger-icon')) {
-        var menu = document.getElementById("hamburger-contents");
+        const menu = document.getElementById("hamburger-contents");
         if (menu.classList.contains('show')) {
             menu.classList.remove('show');
         }
@@ -14,14 +14,14 @@ window.onclick = function(event) {
 
 document.onkeydown = function(evt) {
     evt = evt || window.event;
-    var isEscape = false;
+    let isEscape = false;
     if ("key" in evt) {
         isEscape = (evt.key === "Escape" || evt.key === "Esc");
     } else {
         isEscape = (evt.keyCode === 27);
     }
     if (isEscape) {
-        var menu = document.getElementById("hamburger-contents");
+        const menu = document.getElementById("hamburger-contents");
         if (menu.classList.contains('show')) {
             menu.classList.remove('show');
         }
