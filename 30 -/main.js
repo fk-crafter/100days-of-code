779903625598715bc5a2dfc03fe0748cbee63e93
Fofo <127132293+fk-crafter@users.noreply.github.com>
2024-02-16 14:38:34 +0100

    Create main.js

---
+++ 30 -/main.js
@@ -0,0 +1,51 @@
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// create cube geometry
+const geometry = new THREE.BoxGeometry();
+
+// create ShaderMaterial
+const material = new THREE.ShaderMaterial({
+  vertexShader: `
+    varying vec2 vUv;
+    void main() {
+      vUv = uv;
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    }
+  `,
+  fragmentShader: `
+    uniform vec3 color1;
+    uniform vec3 color2;
+    varying vec2 vUv;
+    void main() {
+      gl_FragColor = vec4(mix(color1, color2, vUv.y), 1.0);
+    }
+  `,
+  uniforms: {
+    color1: { value: new THREE.Color('#bc48ff') },
+    color2: { value: new THREE.Color('blue') }
+  }
+});
+
+// create and add cube
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+// animation function
+const animate = function() {
+  requestAnimationFrame(animate);
+
+  // Rotation du cube
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+};
+
+animate();
