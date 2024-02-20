a2a355697c390a329d9fdc173aa248e401c308de
Fofo <127132293+fk-crafter@users.noreply.github.com>
2024-02-20 17:07:44 +0100

    Create main.js

---
+++ 32 -/main.js
@@ -0,0 +1,7 @@
+const dragAndDropItems = document.getElementById('team-members');
+
+new Sortable(dragAndDropItems, {
+   animation: 350,
+   chosenClass: "team-member-chosen",
+   dragClass: "team-member-drag",
+});
