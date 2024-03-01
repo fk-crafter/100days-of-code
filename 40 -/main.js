6032ee58aec5083402f1af56b385e2ee9b0a73a1
Fofo <127132293+fk-crafter@users.noreply.github.com>
2024-03-01 16:35:47 +0100

    Create main.js

---
+++ 40 -/main.js
@@ -0,0 +1,38 @@
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const particlesGeometry = new THREE.BufferGeometry();
+const particlesCount = 5000;
+
+const posArray = new Float32Array(particlesCount * 3);
+for(let i = 0; i < particlesCount * 3; i++) {
+    posArray[i] = (Math.random() - 0.5) * 5;
+}
+
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(posArray, 3));
+
+const particlesMaterial = new THREE.PointsMaterial({
+    size: 0.025,
+    color: 'white'
+});
+
+const particlesMesh = new THREE.Points(particlesGeometry, particlesMaterial);
+scene.add(particlesMesh);
+
+camera.position.z = 2;
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    particlesMesh.rotation.x += 0.0005;
+    particlesMesh.rotation.y += 0.001;
+
+    renderer.render(scene, camera);
+}
+
+
+
+animate();
