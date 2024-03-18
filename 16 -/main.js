9998101db7445c4fa67076f6ac884ae8587342fe
Fofo <127132293+fk-crafter@users.noreply.github.com>
2024-03-18 19:00:57 +0100

    Update main.js

--- 16 -/main.js
+++ 16 -/main.js
@@ -1,7 +1,7 @@
 document.querySelectorAll('.accordion-button').forEach(button => {
     button.addEventListener('click', function() {
         this.classList.toggle('active');
-        var content = this.nextElementSibling;
+        let content = this.nextElementSibling;
 
         if (this.classList.contains('active')) {
             content.style.maxHeight = content.scrollHeight + "px";
