04b22b95130c8c69313df567926dcf7b43e522ff
Fofo <127132293+fk-crafter@users.noreply.github.com>
2024-03-22 18:44:43 +0100

    Update main.js

--- 48 -/main.js
+++ 48 -/main.js
@@ -1,8 +1,8 @@
 function moveLoadingBar() {
-  var elem = document.getElementById("loadingBar"); 
-  var percentText = document.getElementById("loadingPercent");
-  var width = 0;
-  var id = setInterval(frame, 50);
+  const elem = document.getElementById("loadingBar"); 
+  const percentText = document.getElementById("loadingPercent");
+  let width = 0;
+  let id = setInterval(frame, 50);
 
   function frame() {
     if (width >= 100) {
