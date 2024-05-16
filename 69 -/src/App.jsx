91a3dc113c7953dcc6f2d2105047bae6d7049f56
Fofo <127132293+fk-crafter@users.noreply.github.com>
2024-05-16 23:25:23 +0200

    Add files via upload

--- 69 -/src/App.jsx
+++ 69 -/src/App.jsx
@@ -1,29 +1,58 @@
-import React from 'react';
-import './index.css';
-import Navbar from './components/Navbar';
-import HeroSection from './components/HeroSection';
-import WomenPresentation from './components/WomenPresentation';
-import MenHeroSection from './components/MenHeroSection';
-import MenPresentation from './components/MenPresentation';
+import { useState } from 'react';
+import imgPoke from "./assets/poke.jpg";
+import imgRe4 from "./assets/re4.jpg";
+import imgUncharted from "./assets/uncharted.jpg";
 
+
+import './index.css'
 
 function App() {
+
   return (
-    <div className='App'>
-
-      <Navbar />
-
-      <div className="content">
-        
-        <HeroSection/>
-        <WomenPresentation/>     
-        <MenHeroSection/>
-        <MenPresentation/>
-        
-        <p>Lorem</p>
+    <div id="cards">
+    <div class="card" data-color="blue">
+      <img class="card-front-image card-image" src={imgPoke} />
+      <div class="card-faders">
+        <img class="card-fader card-image" src={imgPoke} />
+        <img class="card-fader card-image" src={imgPoke} />
+        <img class="card-fader card-image" src={imgPoke} />
+        <img class="card-fader card-image" src={imgPoke} />
+        <img class="card-fader card-image" src={imgPoke} />
+        <img class="card-fader card-image" src={imgPoke} />
+        <img class="card-fader card-image" src={imgPoke} />
+        <img class="card-fader card-image" src={imgPoke} />
       </div>
     </div>
-  );
+
+    <div class="card" data-color="green">
+      <img class="card-front-image card-image" src={imgRe4} />
+      <div class="card-faders">
+        <img class="card-fader card-image" src={imgRe4} />
+        <img class="card-fader card-image" src={imgRe4} />
+        <img class="card-fader card-image" src={imgRe4} />
+        <img class="card-fader card-image" src={imgRe4} />
+        <img class="card-fader card-image" src={imgRe4} />
+        <img class="card-fader card-image" src={imgRe4} />
+        <img class="card-fader card-image" src={imgRe4} />
+        <img class="card-fader card-image" src={imgRe4} />
+      </div>
+    </div>
+
+    <div class="card" data-color="brown">
+      <img class="card-front-image card-image" src={imgUncharted} />
+      <div class="card-faders">
+        <img class="card-fader card-image" src={imgUncharted} />
+        <img class="card-fader card-image" src={imgUncharted} />
+        <img class="card-fader card-image" src={imgUncharted} />
+        <img class="card-fader card-image" src={imgUncharted} />
+        <img class="card-fader card-image" src={imgUncharted} />
+        <img class="card-fader card-image" src={imgUncharted} />
+        <img class="card-fader card-image" src={imgUncharted} />
+        <img class="card-fader card-image" src={imgUncharted} />
+      </div>
+    </div>
+  </div>
+  )
 }
 
-export default App;
+export default App
