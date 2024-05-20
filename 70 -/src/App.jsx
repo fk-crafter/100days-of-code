092e56c4dd3c8685b1f3025b4f212d77a1927c29
Fofo <127132293+fk-crafter@users.noreply.github.com>
2024-05-20 15:25:54 +0200

    Update App.jsx

--- 70 -/src/App.jsx
+++ 70 -/src/App.jsx
@@ -2,18 +2,31 @@ import React from 'react';
 import './index.css';
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
+import About from './components/About';
+import Technologies from './components/Technologies';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+
+
 
 function App() {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialised selection:bg-cyan-300 selection:text-cyan-900">
-      <div className="flixed top-0 -z-10 h-full w-full"></div>
-
+      <div className="fixed top-0 -z-10 h-full w-full">
       <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+      </div>
+
+      
 
 
 <div className="container mx-auto px-8">
     <Navbar/>
     <HeroSection/>
+    <About/>
+    <Technologies/>
+    <Projects/>
+    <Contact/>
+    
 
 </div>
     </div>
