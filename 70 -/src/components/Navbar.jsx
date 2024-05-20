7bf3d61502eb16f5a4cb300f56795e0ee9709f9d
Fofo <127132293+fk-crafter@users.noreply.github.com>
2024-05-20 15:23:46 +0200

    Update Navbar.jsx

--- 70 -/src/components/Navbar.jsx
+++ 70 -/src/components/Navbar.jsx
@@ -7,7 +7,6 @@ const Navbar = () => {
     return (  
         <nav className=" mb-20 flex items-center justify-between py-6">
             <div className="flex flex-shrink-0 items-center">
-                <img src={logo} className="w-20 mx-2 w-10" alt="logo" />
             </div>
             <div className="m-8 flex items-center justify-center gap-4 text-2xl">
                 <FaLinkedin/>
