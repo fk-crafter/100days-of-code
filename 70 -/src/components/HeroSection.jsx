ea20585714d4b04ec5e3ebca9e8d67c2835ec639
Fofo <127132293+fk-crafter@users.noreply.github.com>
2024-05-20 15:23:25 +0200

    Update HeroSection.jsx

--- 70 -/src/components/HeroSection.jsx
+++ 70 -/src/components/HeroSection.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import profilePic from "../assets/profilpic.png"
+import {motion} from 'framer-motion'
 
 const HeroSection = () => {
     return (  
@@ -7,14 +8,27 @@ const HeroSection = () => {
             <div className="flex flex-wrap">
                 <div className="w-full lg:w-1/2">
                     <div className="flex flex-col items-center lg:items-start">
-                        <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl" >Fowad Khan</h1>
-                        <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">Front-end Developer</span>
-                        <p className="my-2 max-w-xl py-6 font-light tracking-tight">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa enim iste aliquam omnis, accusantium, quisquam minima, quo nobis tempora itaque repudiandae vitae. Omnis quae explicabo rerum veniam ipsa cumque magnam maiores aliquid ratione sapiente blanditiis consectetur, mollitia, velit quas fugiat.</p>
+                        <motion.h1 
+                        initial={{x: -100, opacity: 0}} 
+                        animate={{x: 0, opacity: 1}} 
+                        transition={{duration: 0.2, delay: 0.2}} 
+                        className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl" >Fowad Khan</motion.h1>
+
+                        <motion.span
+                        initial={{x: -100, opacity: 0}} 
+                        animate={{x: 0, opacity: 1}} 
+                        transition={{duration: 0.2, delay: 0.3}} 
+                        className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">Front-end Developer</motion.span>
+                      
                     </div>
                 </div>
                 <div className="w-full lg:w-1/2 lg:p-8">
                     <div className="flex justify-center">
-                        <img src={profilePic} alt="" />
+                        <motion.img 
+                        initial={{x: 100, opacity: 0}} 
+                        animate={{x: 0, opacity: 1}} 
+                        transition={{duration: 0.2, delay: 0.6}}
+                        src={profilePic} alt="" />
                     </div>
                 </div>
             </div>
