7d82b8ad5fb0ef47d0271a723ef79caf821432f1
Fofo <127132293+fk-crafter@users.noreply.github.com>
2024-07-09 10:28:16 +0200

    Delete 75 - directory

--- 75 -/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-// src/supabaseClient.js
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://rqwanrsjhaunjiafebys.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJxd2FucnNqaGF1bmppYWZlYnlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY5NzA5NDAsImV4cCI6MjAzMjU0Njk0MH0.HwuiAjQIlSdm35zxwRqQUYJs11VJx4lcA-AOkyevI1I';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
